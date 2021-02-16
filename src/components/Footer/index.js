3e595db57dd2330014fbc3ad788368b9e2acb346
Sim kaur <moblue2012@gmail.com>
2021-02-16 18:49:46 -0500

    footer updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,16 +3,23 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer bg-info text-center">
-       <div class="col d-flex justify-content-center">
-            {/* <!-- linkedin--> */}
-            </div><span><a href="https://www.linkedin.com/in/simranksehdev"
-                className="fa fa-linkedin-square d-flex justify-content-center"></a></span>
-            {/* <!--github --> */}
-            <span><a href="https://github.com/simk2020"
-                className="fa fa-github-square d-flex justify-content-center"></a></span>
-       <span>© 2020 Copyright: Simran K Sehdev { new Date().getFullYear() }</span>
-    </footer>
+    <footer className="page-footer fixed-bottom font-small bg-info pt-4">
+    <div className="container-fluid text-center">
+      <div className="row">
+        <div className="col d-flex justify-content-center">
+          <div className="col d-flex justify-content-center">
+            {/* linkedin*/}
+            <span><a href="https://www.linkedin.com/in/simranksehdev" className="fa fa-linkedin-square d-flex justify-content-center" /></span>
+            {/*github */}
+            <span><a href="https://github.com/simk2020" className="fa fa-github-square d-flex justify-content-center" /></span>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright text-center py-3">
+        © 2020 Copyright: Simran K Sehdev
+      </div>
+    </div>
+  </footer>
   );
 }
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,28 @@ import React from "react";
 function About (){
 
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-             <h1>About</h1>
-          </div>
-        </div> 
+      <div className="container"> 
+      <div className="row">
+        <div className="col-md-12">
+          <h1> <strong>About Me!</strong></h1>
+          <hr className="my-3" />
+        </div>
+        <br />
+        <div className="col-md-3">
+          <img src="assets\images\sim.jpg" alt="Simran Sehdev" className="img-thumbnail" />
+        </div>
+        <div className="col-md-8">
+          <p>My name is Simran Sehdev. I am an Experienced Assistant Manager with a demonstrated history of working in the hospitality industry. Skilled in Nonprofit Organizations, Microsoft Word, Event Management, Management, and Software Documentation. </p>
+          <br />
+          <p>Strong professional with a Bachelor of Science (BSc) focused in Business Administration and Management.</p>
+          <p>Here's a link to my Resume.</p>
+          {/* downloadable link */}
+          <a href="/images/resume.jpg" download>
+            <img src="/images/resume.jpg" alt="Resume" />
+          </a>
+        </div>
       </div>
+    </div>
 
     );
 }
